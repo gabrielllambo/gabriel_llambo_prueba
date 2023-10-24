3d457067eae27fb88079b21930c698d00b3317cb
gabrielllambo <gabrielllambo998@gmail.com>
2023-10-24 17:20:23 -0500

    first commit

---
+++ app/src/main/java/uta/fisei/gabriel_llambo_prueba01/TerceraActivity.java
@@ -0,0 +1,14 @@
+package uta.fisei.gabriel_llambo_prueba01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TerceraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tercera);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "gabriel_llambo_prueba01"
+include(":app")
+include(":app")
+ 
